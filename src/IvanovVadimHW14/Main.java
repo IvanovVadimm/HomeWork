d55f158b7a1476f52f39978fe86a357fb351f98c
vadim <vadimivanov0000@gmail.com>
2022-11-18 19:26:24 +0300

    HW15 Работает, но ещё нужно подумать как улучшить (ver.1)

--- src/IvanovVadimHW14/Main.java
+++ src/IvanovVadimHW14/Main.java
@@ -2,56 +2,44 @@ package IvanovVadimHW14;
 
 import javax.swing.text.html.Option;
 import java.util.*;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        TreeMap<Integer, String> idNameMap = new TreeMap<>();
+        TreeMap<Integer, StringBuilder> idNameMap = new TreeMap<>();
 
-        idNameMap.put(1, "Валера1");
-        idNameMap.put(2, "Валера2");
-        idNameMap.put(3, "Валера3");
-        idNameMap.put(4, "Валера4");
-        idNameMap.put(5, "Валера5");
-        idNameMap.put(6, "Валера6");
-        idNameMap.put(7, "Валера7");
-        idNameMap.put(8, "Валера8");
-        idNameMap.put(9, "Валера9");
-        idNameMap.put(10, "Валера10");
-        idNameMap.put(11, "Валера11");
-        idNameMap.put(12, "Валера12");
-        idNameMap.put(13, "Валера13");
-        idNameMap.put(14, "Валера14");
+        idNameMap.put(1, new StringBuilder("Валера1"));
+        idNameMap.put(2, new StringBuilder("Валера2"));
+        idNameMap.put(3, new StringBuilder("Валера3"));
+        idNameMap.put(4, new StringBuilder("Валера4"));
+        idNameMap.put(5, new StringBuilder("Валера5"));
+        idNameMap.put(6, null);
+        idNameMap.put(7, new StringBuilder("Валера7"));
+        idNameMap.put(8, new StringBuilder("Валера8"));
+        idNameMap.put(9, new StringBuilder("Валера9"));
+        idNameMap.put(10, new StringBuilder("Валера10"));
+        idNameMap.put(11, new StringBuilder("Валера11"));
+        idNameMap.put(12, new StringBuilder("Валера12"));
+        idNameMap.put(13, new StringBuilder("Валера13"));
+        idNameMap.put(14, new StringBuilder("Валера14"));
 
-        Optional<TreeMap<Integer, String>> optionalTreeMap = Optional.ofNullable(idNameMap);
+        Optional<TreeMap<Integer, StringBuilder>> optionalTreeMap = Optional.ofNullable(idNameMap);
 
-        LinkedList<String> listOfRightNames = new LinkedList<>();
+        ArrayList<StringBuilder> listOfRightNames = new ArrayList<>();
 
         for (int i = 0; i < idNameMap.size(); i++) {
-            Optional<String> optionalOfName = Optional.ofNullable(idNameMap.get(i));
-            optionalOfName.orElse("There is null value at " + i + " index");
+            Optional<StringBuilder> optionalOfName = Optional.ofNullable(idNameMap.get(i));
             if (optionalOfName.isPresent()) {
                 if ((i == 1) || (i == 2) || (i == 5) || (i == 8) || (i == 9) || (i == 13)) {
                     optionalOfName.filter(x -> x.length() % 2 != 0);
                     listOfRightNames.add(optionalOfName.get());
                 }
+            } else {
+                System.out.println("There is null value at " + i + " index");
             }
         }
-
-        Optional<LinkedList> optionalLinkedList = Optional.ofNullable(listOfRightNames);
-
-        if (!(optionalLinkedList.isPresent())) {
-            System.out.println("List of names is empty");
-        }
-
-        ArrayList<String> listEnd = new ArrayList<>();
-
-        for (String string : listOfRightNames) {
-            StringBuilder reverseString = new StringBuilder(string).reverse();
-            listEnd.add(String.valueOf(reverseString));
-        }
-        for (String strings : listEnd) {
-            System.out.println(strings + " ");
-        }
+        listOfRightNames.stream().map(StringBuilder::reverse).forEach(System.out::println);
     }
 }
