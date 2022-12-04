a79beed53ee5a7413fd72e8f466c66c78b94d76c
vadim <vadimivanov0000@gmail.com>
2022-12-05 00:20:09 +0300

    HW16 Done

--- src/IvanovVadimHW16/Main.java
+++ src/IvanovVadimHW16/Main.java
@@ -1,18 +1,20 @@
 package IvanovVadimHW16;
-
-import javax.sound.midi.Soundbank;
-
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException {
         User user = new User("Вадим");
         user.setBirthDay();
         System.out.println("Отработка метода toString:");
         System.out.println(user);
-        System.out.println("Отработка метода hashCod: "+ user.hashCode());
+        System.out.println("Отработка метода hashCod: " + user.hashCode());
         User userClone = (User) user.clone();
+        System.out.println("Отработка метода toString для клона:");
+        System.out.println(userClone);
         System.out.println("Результат сравнения клонированного и начального объекта:");
         System.out.println(user.equals(userClone));
+        System.out.println("Замена имени в клоне для теста: ");
         userClone.setName("Валера");
+        System.out.println(userClone);
+        System.out.println("Отработка метода hashCod для клона: " + userClone.hashCode());
         System.out.println("Результат сравнения изменённого клонируемого объекта и начального объекта");
         System.out.println(user.equals(userClone));
     }
--- src/IvanovVadimHW16/User.java
+++ src/IvanovVadimHW16/User.java
@@ -3,9 +3,6 @@ package IvanovVadimHW16;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class User implements Cloneable {
     static private int id;
@@ -23,32 +20,12 @@ public class User implements Cloneable {
     }
 
     public void setBirthDay() {
-
-      /*  Scanner scanner = new Scanner(System.in);
-        String date = "";
-        Pattern pattern = Pattern.compile("(0[1-9]|1[0-9]|2[0-9]|3[0-1])\\.((0[1-9])|(1[0-2]))\\.[1-2][0-9][0-9][0-9]");
-
-        boolean flag = true;
-
-        while (flag) {
-            date = scanner.nextLine();
-            Matcher matcher = pattern.matcher(date);
-            if (matcher.matches()) {
-                flag = false;*/
-        int day = (int) ((Math.random() * (31 - 1 + 1) + 1));
-        int month = (int) ((Math.random() * (12 - 1 + 1) + 1));
-        int year = (int) ((Math.random() * (2100 - 1970 + 1) + 1970)); // генерирует года с 1970 по 2100
-        String date = day + "." + month + "." + year;
-        String[] numFromDateLine = date.split("\\.");
-        int dayFromBirthday = Integer.parseInt(numFromDateLine[0]);
-        int monthFromBirthday = Integer.parseInt(numFromDateLine[1]);
-        int yearFromBirthday = Integer.parseInt(numFromDateLine[2]);
+        int dayFromBirthday = (int) ((Math.random() * (31 - 1 + 1) + 1));
+        int monthFromBirthday = (int) ((Math.random() * (12 - 1 + 1) + 1));
+        int yearFromBirthday = (int) ((Math.random() * (2100 - 1970 + 1) + 1970)); // генерирует года с 1970 по 2100
         LocalDate localDate = LocalDate.of(yearFromBirthday, monthFromBirthday, dayFromBirthday);
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd MMMM yyyy");
         this.birthDay = dateTimeFormatter.format(localDate);
-            /*} else {
-                System.out.println("Не верный формат даты рождения. Попробуйте снова...");
-            }*/
     }
 
     @Override
@@ -76,5 +53,4 @@ public class User implements Cloneable {
     public int hashCode() {
         return 17 * 31 * birthDay.hashCode() * name.hashCode();
     }
-
 }
